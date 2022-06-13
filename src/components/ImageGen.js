d3d121c9dfa019eb41c008c1b73f11a356e04162
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 18:56:52 +0100

    Create a function sending the random image request.

--- src/components/ImageGen.js
+++ src/components/ImageGen.js
@@ -1,6 +1,17 @@
 import './ImageGen.css';
 import { useState } from "react";
 
+async function getImg() {
+  const url = 'https://api.thecatapi.com/v1/images/search'
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'x-api-key': process.env.REACT_APP_CAT_API_KEY
+    }
+  });
+  return response.json();
+}
+
 function ImageGen() {
   const [imgLink, setImgLink] = useState("");
 
