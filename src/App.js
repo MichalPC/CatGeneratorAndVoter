46128c2f2c4bff2e1655d6ebd61185c65bc270d9
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 21:50:17 +0100

    Add the header component to the main app component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route, Router, NavLink } from 'react-router-dom';
 import Home from './components/Home';
+import Header from './components/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <div className="main">
         <Routes>
           <Route path="/" element={< Home/ >} />
