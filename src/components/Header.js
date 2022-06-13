06146dfbf932d32d37da5ed77f17b4e19811fed4
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 22:21:10 +0100

    Add a nav bar to the header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,10 @@ function Header() {
   return (
     <div className="Header">
       <h1> Cat API App </h1>
+      <div className="nav-bar">
+        <a href='#'> Home </a>
+        <a href='#'> Random Image </a>
+      </div>
     </div>
   );
 }
