9dc1086c4ea8accfc2a1d54eb3585063d2347d44
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 17:50:43 +0100

    Create files for random image generation component.

---
+++ src/components/ImageGen.js
@@ -0,0 +1,12 @@
+import './ImageGen.css';
+
+function ImageGen() {
+  return (
+    <div className="ImageGen">
+      <div className="main">
+      </div>
+    </div>
+  );
+}
+
+export default ImageGen;
