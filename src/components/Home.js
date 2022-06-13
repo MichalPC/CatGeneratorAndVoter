68483d4eb3fcd932fe142e3c426cf372d10702b7
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 18:45:44 +0100

    Display ImageGen components on the home page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import './Home.css';
+import ImageGen from "./ImageGen"
 
 function Home() {
   return (
     <div className="Home">
       <div className="main">
+        <ImageGen />
       </div>
     </div>
   );
