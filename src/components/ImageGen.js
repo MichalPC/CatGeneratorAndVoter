1b5dafb9f6e49ce57af85b10712a0fa49ba92e5b
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 18:48:34 +0100

    Create a state hook for the current random image link.

--- src/components/ImageGen.js
+++ src/components/ImageGen.js
@@ -1,6 +1,9 @@
 import './ImageGen.css';
+import { useState } from "react";
 
 function ImageGen() {
+  const [imgLink, setImgLink] = useState("");
+
   return (
     <div className="ImageGen">
       <div className="main">
