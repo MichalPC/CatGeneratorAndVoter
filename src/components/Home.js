3d5d5a8a3f0be41a7d8205e8c1bcbf8e44829da4
MichalPC <michal.cyganek.2000@gmail.com>
2022-06-13 17:16:56 +0100

    Initial React App Setup.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <div className="main">
+      Home
+      </div>
+    </div>
+  );
+}
+
+export default Home;
